c9692ede813056518e61f585e88103470b1b23e9
jbrsc <94963382+jbrsc@users.noreply.github.com>
2023-10-03 07:09:48 +0200

    Update rpm_command.cpp
    Due to tests using the VESC tools, the ROS input and VESC input could be observed. 
    The previous value of 3000*ros_msg.data would result in 1 in input equal to 430 rpm (3000/7)

--- uavcan_ros_bridge/src/ros_to_uav/rpm_command.cpp
+++ uavcan_ros_bridge/src/ros_to_uav/rpm_command.cpp
@@ -14,7 +14,7 @@ template <>
 bool convert(const acomar_thruster::ThrusterCmd& ros_msg, uavcan::equipment::esc::RPMCommand& uav_msg)
 {
     uav_msg.rpm.resize(1);
-    uav_msg.rpm[0] = ros_msg.data*3000; //TODO: Parameterize this
+    uav_msg.rpm[0] = ros_msg.data*7; //As T500 Thrusters have 7 pole pairs the input is now correctly sent as VESC believes the input is in ERPM (was 3000). 
     return true;
 }
 
