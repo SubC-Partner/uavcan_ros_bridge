210a88056bbda2b29497ae121e9038bad3e0f4ce
Christian Mai <chrimai@energy.aau.dk>
2023-10-19 18:40:51 +0200

    Update rpm_command.cpp with multi ESC example

--- uavcan_ros_bridge/src/ros_to_uav/rpm_command.cpp
+++ uavcan_ros_bridge/src/ros_to_uav/rpm_command.cpp
@@ -14,7 +14,9 @@ template <>
 bool convert(const acomar_thruster::ThrusterCmd& ros_msg, uavcan::equipment::esc::RPMCommand& uav_msg)
 {
     uav_msg.rpm.resize(1);
-    uav_msg.rpm[0] = ros_msg.data*7; //As T500 Thrusters have 7 pole pairs the input is now correctly sent as VESC believes the input is in ERPM (was 3000). 
+    uav_msg.rpm[0] = ros_msg.data*7; //ESC 0, As T500 Thrusters have 7 pole pairs the input is now correctly sent as VESC believes the input is in ERPM (was 3000). 
+    //uav_msg.rpm[1] = ros_msg.data*7 //ESC 1
+    //uav_msg.rpm[2] = ros_msg.data*7 //ESC 2 etc.
     return true;
 }
 
